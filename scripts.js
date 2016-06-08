1487043f6462001569821f90061b989449a3b561
acuervoa <kuerbo@gmail.com>
2016-06-08 22:25:38 +0200

    change to table responsive

--- scripts.js
+++ scripts.js
@@ -8,16 +8,20 @@ $(document).ready(function(){
 		usersNames.forEach(function(userName){
 
 			var streamStatus;
+			var statusRow;
 				
 			 $.getJSON('https://api.twitch.tv/kraken/streams/' + userName + "?callback=", function(data) {
 			 	console.log(data);
 
 			 	if (data.stream === null) {
 					streamStatus = 'offline';
+					statusRow = 'table-active';
 				} else if (data.stream === undefined) {
 					streamStatus = 'undefined';
+					statusRow = 'table-warning';
 				} else {
 					streamStatus = 'online';
+					statusRow = 'table-success';
 				}
 			});
 				
@@ -25,19 +29,19 @@ $(document).ready(function(){
 				
 				//console.log(data);
 
-				var codeHTML = '<tr>';
+				var codeHTML = '<tr class="' + statusRow +  '">';
 
-				codeHTML += '<td class="' + ( streamStatus !== 'undefined' ? streamStatus : '') + '">' ;
+				codeHTML += '<td>' ;
 				codeHTML += data.display_name;
 				codeHTML += '</td>';
 
-				codeHTML += '<td>';
-
-				codeHTML += '<img src="' + data.logo + '" class="logo" />'
+				codeHTML += '<td>' ;
+				codeHTML += '<img src="' + (data.logo !== null ? data.logo : 'https://dummyimage.com/50x50/ecf0e7/5c5457.jpg&text=0x3F') + '" class="logo" />';
 				codeHTML += '</td>';
 
-				codeHTML += '<td class="' + ( streamStatus !== 'undefined' ? streamStatus : '') + '">' ;
-				codeHTML += data.display_name;
+				codeHTML += '<td>' ;
+				codeHTML += '<span class="status">' + ( streamStatus !== 'undefined' ? streamStatus : '') + "</span>"; 
+				codeHTML += '<span class="description">' + (data.status !== null ? data.status : '') + "</span>";
 				codeHTML += '</td>';
 
 				codeHTML += "</tr>";
